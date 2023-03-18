070184bebb2108cb523b57094dbddd2b0e28ac68
Eros Menin <eros.menin@gmail.com>
2023-03-18 17:51:58 +0100

    modified thias creation

---
+++ Controller/Order.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+
+/*
+{
+    "hamburgers":[
+       {
+          "name":"Classic",
+          "price":6.99,
+          "ingredients":[
+             {
+                "name": "bun",
+                "amount": 2
+             },
+            //  "beef patty",
+            //  "lettuce",
+            //  "tomato",
+            //  "sauce",
+            //  "cheese"
+          ]
+       },
+       {
+          "name":"Classic",
+          "price":6.99,
+          "ingredients":[
+             {
+                "name": "bun",
+                "amount": 2
+             },
+            //  "beef patty",
+            //  "lettuce",
+            //  "tomato",
+            //  "sauce",
+            //  "cheese"
+          ]
+       },
+    ]
+}
+*/
