5c1c2715fbc77948def4c32b8bf0c150839a9d47
Alberto Barison <alberto.barison@itiszuccante.edu.it>
2023-03-18 17:44:27 +0100

    Json method shall work now

--- Controller/Ingredients.php
+++ Controller/Ingredients.php
@@ -1,18 +1,10 @@
 <?php
-include 'Models/Ingrediente.php';
- $ingredient = new Ingrediente;
- ingredientsToJson($ingredient ->getAll());
+include '../Models/Ingrediente.php';
+$ingredient = new Ingrediente;
+print_r(ingredientsToJson($ingredient->getAll()));
 
 
-function ingredientsToJson($ingredients) {
-    $jsonIngredients= array();
-    foreach ($ingredients as $thisIngredient) {
-      $jsonArray[] = array(
-        'id' => $thisIngredient['id'],
-        'prezzo' => $thisIngredient['nome'],
-        'immagine' => $thisIngredient['prezzo'],
-        'nome' => $thisIngredient['immagine'],
-      );
-    }
-    return json_encode($jsonIngredients);
-  }
+function ingredientsToJson($ingredients)
+{
+  return json_encode($ingredients);
+}
