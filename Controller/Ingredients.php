1da0e27a155c4a307cd9662795ca71e63fbbd595
Alberto Barison <alberto.barison@itiszuccante.edu.it>
2023-03-18 15:56:36 +0100

    Method to convert ingredients to JSON done

---
+++ Controller/Ingredients.php
@@ -0,0 +1,18 @@
+<?php
+include 'Models/Ingrediente.php';
+ $ingredient = new Ingrediente;
+ ingredientsToJson($ingredient ->getAll());
+
+
+function ingredientsToJson($ingredients) {
+    $jsonIngredients= array();
+    foreach ($ingredients as $thisIngredient) {
+      $jsonArray[] = array(
+        'id' => $thisIngredient['id'],
+        'prezzo' => $thisIngredient['nome'],
+        'immagine' => $thisIngredient['prezzo'],
+        'nome' => $thisIngredient['immagine'],
+      );
+    }
+    return json_encode($jsonIngredients);
+  }
