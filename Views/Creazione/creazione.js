9beb57602d20ea5040132c86408a316cd2c0128d
Vianello Nicolo <nicolo.vianello@cz.lan>
2023-03-22 09:54:12 +0100

    grafica nettamente superiore a quella di thias

--- Views/Creazione/creazione.js
+++ Views/Creazione/creazione.js
@@ -1,7 +1,7 @@
 var price = 0;
 let list = {};
 list['ingredients'] = {};
-var data = '[{"id":1,"prezzo":0.5,"immagine":"Pane-bianco.png","nome":"Pane Bianco","tipo":"pane"},{"id":2,"prezzo":1.25,"immagine":"wurstel.png","nome":"Wurstel","tipo":"carne"},{"id":3,"prezzo":0.8,"immagine":"Pane-integrale.png","nome":"Pane Integrale","tipo":"pane"},{"id":4,"prezzo":1.25,"immagine":"hamburger.png","nome":"Wurstel","tipo":"carne"}]';
+var data = '[{"id":1,"prezzo":0.5,"immagine":"paninohamburgernorm.png","nome":"Pane Bianco","tipo":"pane"},{"id":2,"prezzo":1.25,"immagine":"wurstel.jpg","nome":"Wurstel","tipo":"carne"},{"id":3,"prezzo":0.8,"immagine": "paneintegrale.jpg","nome":"Pane Integrale","tipo":"pane"},{"id":4,"prezzo":1.25,"immagine":"hamburger.jpg","nome":"Wurstel","tipo":"carne"}]';
 
 
 //aggiunta dell'ingrediente al panino su richiesta dell'utente
@@ -42,49 +42,11 @@ function getHtml() {
     .then(data =>{*/
     data = JSON.parse(data);
     data.forEach(element => {
-        var div1 = document.createElement('div');
-        div1.className = 'singlebutton';
-        var div2 = document.createElement('div');
-        div2.className = 'ingrediente';
-        var image = document.createElement('img');
-        image.src = element.immagine;
-        image.width = 200;
-        image.height = 180;
-        var p = document.createElement('p');
-        p.appendChild(document.createTextNode(element.nome + ' ' + element.prezzo));
-        var button1 = document.createElement('button');
-        var button2 = document.createElement('button');
-        button1.className = 'piu';
-        button2.className = 'meno';
-        var div3 = document.createElement('div');
-        div3.className = 'piumeno';
-        button1.onclick = function () {
-            getIngredients(element.nome, 1, element.prezzo);
-        };
-        var imgp = document.createElement('img');
-        imgp.src = 'https://img.favpng.com/4/5/15/computer-icons-symbol-addition-plus-and-minus-signs-png-favpng-NB4npaRQPe9p0HHCsuQNV1YD2.jpg';
-        imgp.width = 50;
-        imgp.height = 50;
-        button1.appendChild(imgp);
-        var imgm = document.createElement('img');
-        imgm.src = 'https://icons-for-free.com/download-icon-math+minus+sign+stop+icon-1320184998453384308_512.png';
-        imgm.width = 50;
-        imgm.height = 50;
-        button2.appendChild(imgm);
-        button2.onclick = function () {
-            getIngredients(element.nome, -1, element.prezzo);
-        };
-        div3.appendChild(button1);
-        div3.appendChild(button2);
-        div2.appendChild(image);
-        div2.appendChild(document.createElement('br'));
-        div2.appendChild(p);
-        div1.appendChild(div2);
-        div1.appendChild(div3);
-        console.log(div1);
-        document.getElementById(element.tipo).appendChild(div1);
+        document.querySelector('#' + element.tipo).innerHTML += buildElement(element.nome, element.prezzo, element.immagine); 
     });
 
-}/*
-    )
-}*/
+}
+
+function buildElement(name, price, image){
+    return '<div><div><img src="Image/'+ image +'"></div><div><p id="name">'+ name +'</p><span><button>-</button><button>+</button></span><p id="price"><span id="amount">'+ price +'</span>€</p></div></div>'; 
+}
