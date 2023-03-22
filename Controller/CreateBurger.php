c9d8572ec3cad5017fb510ecf1b67fe693bf7463
Dotoli Francesco <francesco.dotoli@cz.lan>
2023-03-22 09:36:17 +0100

    delate all

--- Controller/CreateBurger.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-session_start();
-
-header('Location: ../Views/Creazione/creazione.html');
--- Controller/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    if (isset($_SESSION['auth']) && $_SESSION['auth']) {
-        header('Location: ../Views/Mainpage.html');
-    }else{
-        header('Location: ../Views/Login/LoginPage.html');
-    }
-?>
