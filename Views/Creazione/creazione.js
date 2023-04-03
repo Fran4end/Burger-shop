95f09d4ee42c2d952d591b6d12601120ce47cef3
Dotoli Francesco <francesco.dotoli@cz.lan>
2023-04-03 10:47:16 +0200

    update creazione.js

--- Views/Creazione/creazione.js
+++ Views/Creazione/creazione.js
@@ -3,16 +3,45 @@ let list = {};
 list['ingredients'] = {};
 
 //aggiunta dell'ingrediente al panino su richiesta dell'utente
-function getIngredients(ingrediente, ammount, prezzo) {
+function getIngredients(ingrediente, prezzo, ammount, classe) {
     if (list['ingredients'][ingrediente] == null && ammount == 1) {
         list['ingredients'][ingrediente] = 1;
         price += prezzo;
+        document.querySelector('#'+ingrediente).innerHTML+='<button id="number'+ingrediente+'">'+list["ingredients"][ingrediente]+'</button>';
     } else if ((list['ingredients'][ingrediente] == 0 || list['ingredients'][ingrediente] == null) && ammount == -1) {
         alert('Non ci sono ingredienti: ' + ingrediente);
 
     } else {
         list['ingredients'][ingrediente] += ammount;
         price += prezzo;
+        document.getElementById(ingrediente).removeChild(document.getElementById('number'+ingrediente));
+        document.querySelector('#'+ingrediente).innerHTML+='<button id="number'+ingrediente+'">'+list["ingredients"][ingrediente]+'</button>';
+        if(list['ingredients'][ingrediente]==0){
+            delete list['ingredients'][ingrediente];
+            document.getElementById(ingrediente).removeChild(document.getElementById('number'+ingrediente));
+            console.log(list['ingredients']);
+        } 
+    }
+    if(classe=='pane'){
+        let e = document.getElementsByClassName('pane');
+        console.log(e);
+        let i=-1;
+        if(ammount==1){
+            for(i=0; i<e.length;i++){
+                console.log(e[i].id);
+                if(!(e[i].id==('remove'+ingrediente))){
+                    e[i].disabled=true;
+                }
+            }
+        }
+        if(ammount==-1){
+            for(i=0;i<e.length;i++){
+                console.log(e[i].id);
+                if(!(e[i].id==('remove'+ingrediente))){
+                    e[i].disabled=false;
+                }
+            }
+        }
     }
     console.log(list['ingredients']);
 }
@@ -40,13 +69,13 @@ function getHtml() {
         .then(data => {
             console.log(data);
             data.forEach(element => {
-                document.querySelector('#' + element.categoria).innerHTML += buildElement(element.nome, element.prezzo.toFixed(2), element.immagine);
+                document.querySelector('#' + element.categoria).innerHTML += buildIngredients(element.nome, element.prezzo.toFixed(2), element.immagine, element.categoria);
             });
         })
 }
 
-function buildElement(name, price, image) {
-    return '<div><div><img src=' + image + '></div><div><p id="name">' + name.charAt(0).toUpperCase() + name.replaceAll("_", " ").slice(1) + '</p><span><button>-</button><button>+</button></span><p id="price"><span id="amount">' + price + '</span>€</p></div></div>';
+function buildIngredients(name, price, image, classe) {
+    return '<div><div><img src=' + image + '></div><div><p id="name">' + name.charAt(0).toUpperCase() + name.replaceAll("_", " ").slice(1) + '</p><span id="'+name+'"><button id="remove'+name+'" class="'+classe+'" onclick="getIngredients(`'+name+'`,'+price+','+'-1'+',`'+classe+'`)" >-</button><button id="add'+name+'" class="'+classe+'" onclick="getIngredients(`'+name+'`,'+price+','+'1'+',`'+classe+'`)">+</button></span><p id="price"><span id="amount">' + price + '</span>€</p></div></div>';
 }
 
 //Avvia un pop-up dove verrà chiesto il nome del panino e poi mandato alla pagina chekout
@@ -61,7 +90,7 @@ function goToChekout() {
         confirmButtonText: 'vai al checkout',
         showLoaderOnConfirm: true,
         preConfirm: (panino) => {
-            //TODO: chiamare il file php per passare il json e al chekout
+            //TODO: chiamare il file php per passare il json e andare al chekout
         },
         allowOutsideClick: () => !Swal.isLoading()
     })
