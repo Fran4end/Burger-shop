d1c8e9d8bd3f2b82124c08f434ae9761a7fd8135
Eros Menin <eros.menin@gmail.com>
2023-04-07 17:20:45 +0200

    modifed redirects in Checkout.php

--- Controller/Checkout.php
+++ Controller/Checkout.php
@@ -11,78 +11,82 @@ include 'logged.php';
 
 session_start();
 // if the user isn't logged redirects to login
-checkLogin();
+if (!isset($_SESSION['user'])) {
+    echo json_encode(["result" => false]);
+}else{
+    // if the user correctly sent the json containing the panini
+    if (!empty(json_decode(file_get_contents('php://input'), true))) {
+        // decodes the json and clear the SESSION
+        $json = json_decode(file_get_contents('php://input'), true);
+        
+        // gets the User form the db
+        $user = new Utente('a', 'a');  //lazy initialization
+        $user_id = $_SESSION['user']['User_ID'];
+        $user->getUtenteById($user_id);
 
-// if the user correctly sent the json containing the panini
-if (!empty(json_decode(file_get_contents('php://input'), true))) {
-    // decodes the json and clear the SESSION
-    $json = json_decode(file_get_contents('php://input'), true);
-    
-    // gets the User form the db
-    $user = new Utente('a', 'a');  //lazy initialization
-    $user_id = $_SESSION['user']['User_ID'];
-    $user->getUtenteById($user_id);
+        // creates the Ordine
+        $order = new Ordine();
+        $amount = 0;
+        foreach ($json as $value) {
+            $amount += $value['prezzo'] * $value['quantità'];
+        }
 
-    // creates the Ordine
-    $order = new Ordine();
-    $amount = 0;
-    foreach ($json as $value) {
-        $amount += $value['prezzo'] * $value['quantità'];
-    }
+        // if the user hasn't enough 'schei' the server doesn't do anything
+        if ($amount > $user->getSaldo()) {
+            ?>
+                <script>
+                    if (!alert('no schei'))
+                        document.location = 'Annulla.php';
+                </script>
+            <?php
+            header('Location: ../Views/Home/home.html');
+            return;
+        } else { // otherwise decrement the salt (doesn't affect db)
+            $user->updateSaldo(-$amount, $user_id);
+        }
 
-    // if the user hasn't enough 'schei' the server doesn't do anything
-    if ($amount > $user->getSaldo()) {
+        $order->setPrezzo($amount);
+        $order_id = $order->createOrder($user_id);
+
+        //creates the Panini
+        $ingrediente = new Ingrediente();
+        $all_ingredients = $ingrediente->getAll();
+
+        $panino = new Panino();
+
+        foreach ($json as $burger) {
+            // creates the same Panino (and all the Preparazioni) for panino['quantità'] times 
+            for ($i = 0; $i < $burger['quantità']; $i++) {
+
+                $panino->setNome($burger['nome']);
+                $panino->setPrezzo($burger['prezzo']);
+                $panino->createBurger($order_id);
+
+                // creates all the Preparazioni
+                foreach ($burger['ingredienti'] as $ing) {
+                    $panino->addIngrediente(
+                        getIngredientId($ing['nome'], $all_ingredients),
+                        $order_id,
+                        $ing['quantità'],
+                    );
+                }
+            }
+        }
+
+        // deletes the order in the SESSION and redirects to home
+        unset($_SESSION['order']);
+        echo json_encode(["result" => true]);
+    } else { //if the data of the order don't exist
         ?>
             <script>
-                if (!alert('no schei'))
+                if (!alert('nessun parametro passato, ci scusiamo, ma i nostri programmatori sono sottopagati ;)'))
                     document.location = 'Annulla.php';
             </script>
         <?php
-        header('Location: ../Views/Home/home.html');
-        return;
-    } else { // otherwise decrement the salt (doesn't affect db)
-        $user->updateSaldo(-$amount, $user_id);
     }
-
-    $order->setPrezzo($amount);
-    $order_id = $order->createOrder($user_id);
-
-    //creates the Panini
-    $ingrediente = new Ingrediente();
-    $all_ingredients = $ingrediente->getAll();
-
-    $panino = new Panino();
-
-    foreach ($json as $burger) {
-        // creates the same Panino (and all the Preparazioni) for panino['quantità'] times 
-        for ($i = 0; $i < $burger['quantità']; $i++) {
-
-            $panino->setNome($burger['nome']);
-            $panino->setPrezzo($burger['prezzo']);
-            $panino->createBurger($order_id);
-
-            // creates all the Preparazioni
-            foreach ($burger['ingredienti'] as $ing) {
-                $panino->addIngrediente(
-                    getIngredientId($ing['nome'], $all_ingredients),
-                    $order_id,
-                    $ing['quantità'],
-                );
-            }
-        }
-    }
-
-    // deletes the order in the SESSION and redirects to home
-    unset($_SESSION['order']);
-    header('Location: ../Views/Home/home.html');
-} else { //if the data of the order don't exist
-    ?>
-        <script>
-            if (!alert('nessun parametro passato, ci scusiamo, ma i nostri programmatori sono sottopagati ;)'))
-                document.location = 'Annulla.php';
-        </script>
-    <?php
 }
+
+
 
 /**
  * Iterates an array of Ingredienti to obtain the id of the Ingredient by the name.
