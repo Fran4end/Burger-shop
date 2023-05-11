0c3582662f4f6369f294f20dd51985394af8098a
Fran4end <francyd04@gmail.com>
2023-05-11 09:11:06 +0200

    Update Ingredients.php

--- Ingredients.php
+++ Ingredients.php
@@ -4,6 +4,8 @@
  * Returns a json containing all the ingredients in the db
  */
 
+header("content-type: application/json");
+
 include 'Models/Ingrediente.php';
 $ingredient = new Ingrediente();
 echo json_encode($ingredient->getAll());
