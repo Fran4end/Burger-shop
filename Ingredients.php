54b03c89d106e3b90b249c15dfc29bb194f85f5c
Eros <80034768+ErosM04@users.noreply.github.com>
2023-05-11 09:15:52 +0200

    Update Ingredients.php

--- Ingredients.php
+++ Ingredients.php
@@ -4,7 +4,7 @@
  * Returns a json containing all the ingredients in the db
  */
 
-header('Content-Type: application/json; charset=utf-8');
+header('Content-Type:application/json; charset=utf-8');
 
 include 'Models/Ingrediente.php';
 $ingredient = new Ingrediente();
